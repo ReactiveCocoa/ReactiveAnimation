64c76ae9a374d6ff2948b413da5edb93df154c1d
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2015-03-22 20:02:35 -0700

    Bootstrap project and workspace

---
+++ ReactiveAnimation/ReactiveAnimation.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveAnimation.h
+//  ReactiveAnimation
+//
+//  Created by Justin Spahr-Summers on 2015-03-22.
+//  Copyright (c) 2015 ReactiveCocoa. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ReactiveAnimation.
+FOUNDATION_EXPORT double ReactiveAnimationVersionNumber;
+
+//! Project version string for ReactiveAnimation.
+FOUNDATION_EXPORT const unsigned char ReactiveAnimationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveAnimation/PublicHeader.h>
+
+
