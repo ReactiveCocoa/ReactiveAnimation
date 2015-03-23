c87669c532c731451dd4d4079b671edba7da9cac
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2015-03-22 20:13:17 -0700

    Fix test running

---
+++ ReactiveAnimationTests/SwiftJunk.swift
@@ -0,0 +1,15 @@
+//
+//  SwiftJunk.swift
+//  ReactiveAnimation
+//
+//  Created by Justin Spahr-Summers on 2015-03-22.
+//  Copyright (c) 2015 ReactiveCocoa. All rights reserved.
+//
+
+import Foundation
+import LlamaKit
+import ReactiveCocoa
+import XCTest
+
+// Forces Xcode to link the Swift stdlib.
+final class SwiftJunkSpec: XCTestCase {}
